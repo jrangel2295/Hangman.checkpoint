cdeede9590635ff2c343c752ecfb1e61fb4f1350
MHargrove96 <86787734+MHargrove96@users.noreply.github.com>
2022-01-24 20:12:30 -0600

    added the guess and hangman functions

--- main.js
+++ main.js
@@ -17,46 +17,70 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+let guess = "";
+let solution = [];
+let guessArray = [];
 
-const hangMan = () => {};
+const hangMan = (guess) => {
+  console.log(solution);
+  if (solution.includes(guess)) {
+    console.log("letter is in the solution");
+    return guess;
+  }
+  if (!solution.includes(guess)) {
+    console.log("The letter is not in the solution");
+    return guess;
+  }
+};
 
 const newWord = () => {
   let randomNumber = Math.floor(Math.random() * 50);
-  let solution = randomWordArry[randomNumber];
+  solution = randomWordArry[randomNumber].split("");
   console.log(randomNumber);
   console.log(solution);
+  console.log(typeof solution);
   return solution;
 };
+newWord();
+
+const guessWord = (guess) => {
+  console.log(guess);
+  return guess;
+};
+// guessWord("a");
 
 // ----------- test ----------- ----------- test ----------- ----------- test ----------- ----------- test -----------
 const getPrompt = () => {
   rl.question("word", (answer) => {
+    hangMan(answer);
     console.log(answer);
     getPrompt();
   });
 };
 
-if (typeof describe === "function") {
-  // describe('BankAccount', function(){
-  //     it("Should have an account number, owner's name and a transaction list", function(){
-  //         const bankAccount1 = new BankAccount('1234567', 'Michael H.');
-  //         assert.equal(bankAccount1.accountNumber, '1234567');
-  //         assert.equal(bankAccount1.owner, 'Michael H.')
-  //         assert.equal(bankAccount1.transactions.length, 0)
-  //         assert.equal(bankAccount1.balance(), 0)
-  //     })
-  // })
-//   describe("hangMan", function () {
-//     it("should ", function () {
-//       assert.equal(hangMan(), 3);
+getPrompt();
+
+// if (typeof describe === "function") {
+//   // describe('BankAccount', function(){
+//   //     it("Should have an account number, owner's name and a transaction list", function(){
+//   //         const bankAccount1 = new BankAccount('1234567', 'Michael H.');
+//   //         assert.equal(bankAccount1.accountNumber, '1234567');
+//   //         assert.equal(bankAccount1.owner, 'Michael H.')
+//   //         assert.equal(bankAccount1.transactions.length, 0)
+//   //         assert.equal(bankAccount1.balance(), 0)
+//   //     })
+//   // })
+//   //   describe("hangMan", function () {
+//   //     it("should ", function () {
+//   //       assert.equal(hangMan(), 3);
+//   //     });
+//   //   });
+
+//   describe("newWord", function () {
+//     it("should produce a random word from the randomWordArray", function () {
+//       assert.equal(typeof (newWord(),'object')); //how to test this if the answer is going to be a random word from the list?
 //     });
 //   });
-
-  describe("newWord", function () {
-    it("should produce a random word from the randomWordArray", function () {
-      assert.equal(typeof(newWord(), string)); //how to test this if the answer is going to be a random word from the list? 
-    });
-  });
-} else {
-  getPrompt();
-}
+// } else {
+//   getPrompt();
+// }
